20812eda4f73ccc38d03cb7e7e852cc85304bb84
Sophia Thapa <sophia.thapa55@gmail.com>
2025-06-30 17:29:29 +0545

    display keyboard

--- react/hangman_game/src/app/page.tsx
+++ react/hangman_game/src/app/page.tsx
@@ -8,12 +8,18 @@ let letters = [
   'v', 'w', 'x', 'y', 'z'
 ];
 
+const keyboard = [
+    ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+    ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+    ['Z', 'X', 'C', 'V', 'B', 'N', 'M']
+]
 
 export default function Home() {
   const [ guessWord,setguessWord] = useState('_______')
   const [ image,setimage] = useState(1)
   const [index,setIndex]  = useState([])
-  const correctWord = 'hangman'
+  const[color,setColor] = useState('bg-black')
+  const correctWord = 'HANGMAN'
   const handleGuess =(val)=>{
       const isIncluded = correctWord.includes(val)
     if (isIncluded){
@@ -23,35 +29,40 @@ export default function Home() {
         }).filter(item=> item || item == 0)
 
         setIndex([...index, ...matchedIndex])
-        guessWord.split("").map((val,id)=>{
-        })
+        
     } 
 
     else {     
         setimage(image+1)
+        setColor('bg-gray-500')
     }
   } 
 
   return (
    <div className=" justify-center m-10 ">
-  
     <div className="mt-20">
     <img src={`hang_${image}.gif`} alt='hangman'/>
     </div>
-    <div className="mt-10 flex  flex-col">
-      <div className="flex gap-3">
+    <div className=" flex  flex-col">
+      <div className="flex gap-3 justify-center">
        {guessWord.split('').map((val,id)=>{
      return (<div key ={id} className="">
            {index.includes(id) ? correctWord[id] : '_'}
      </div>)
   })}
   </div>
-      <div className=" flex mt-6">
-      {letters.map((item,id)=>{
-        return (<div key = {id} className="bg-black text-white p-2 m-2" onClick={()=>handleGuess(item)} >
-         {item}
-        </div>)
-      })}
+      <div className=" mt-15">
+      {(image <7 && correctWord.length !== index.length) && keyboard.map((val,id)=>{
+        return(<div key = {id} className="flex justify-center item-center ">{
+
+          val.map((item,idx)=>{
+          return(<div key = {idx} className={`p-2 m-2 bg-black text-white`}  onClick={()=>handleGuess(item)}>
+                {item}
+            </div>
+          )
+        })
+         }</div>
+      )})}
      </div>
     </div>
    </div>
